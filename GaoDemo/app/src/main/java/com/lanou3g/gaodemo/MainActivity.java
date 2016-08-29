9785851c50d13551ab7ed5ea06d6d5d0bcd0d47d
高鹏 <1598039446@qq.com>
2016-08-29 14:51:13 +0800

    组长写

--- GaoDemo/app/src/main/java/com/lanou3g/gaodemo/MainActivity.java
+++ GaoDemo/app/src/main/java/com/lanou3g/gaodemo/MainActivity.java
@@ -2,9 +2,12 @@ package com.lanou3g.gaodemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
 //
 public class MainActivity extends AppCompatActivity {
 
+    private TextView n;//组长写代码
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
